52bdb4c4b782b27a6a6d5afc5fb8f035d5e1ff9e
hainet <20400616+hainet@users.noreply.github.com>
2018-02-22 00:00:28 +0900

    @RequestHeaderの利用サンプルを追加

--- src/main/java/com/hainet/spring/web/sample/web/RequestController.java
+++ src/main/java/com/hainet/spring/web/sample/web/RequestController.java
@@ -1,7 +1,10 @@
 package com.hainet.spring.web.sample.web;
 
 import lombok.Data;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class RequestController {
@@ -18,14 +21,32 @@ public class RequestController {
         return "@PathVariable: " + value;
     }
 
-    // curl -XGET localhost:8080/request-body -H 'Content-type: application/json' -d '{"value": "baz"}'
+
+    // curl -XGET http://localhost:8080/request-header -H 'Header-value: baz'
+    @GetMapping("/request-header")
+    public String requestHeaderTest(
+            @RequestHeader("Header-value") final String headerValue,
+            @RequestHeader final Map<String, Object> map,
+            @RequestHeader final HttpHeaders httpHeaders,
+            @RequestHeader(value = "Not-required", required = false) final String notRequired,
+            @RequestHeader(value = "", defaultValue = "This is default value.") final String defaultValue) {
+        return
+                "@RequestHeader\n" +
+                "  Header-value: " + headerValue + "\n" +
+                "  Map<String, Object>: " + map + "\n" +
+                "  HttpHeaders: " + httpHeaders + "\n" +
+                "  required = false: " + notRequired + "\n" +
+                "  defaultValue = \"This is default value.\": " + defaultValue + "\n";
+    }
+
+    // curl -XGET http://localhost:8080/request-body -H 'Content-type: application/json' -d '{"value": "baz"}'
     @GetMapping("/request-body")
-    public String requestBodyTest(@RequestBody final RequestForm form) {
+    public String requestBodyTest(@RequestBody final RequestBodyForm form) {
         return "@RequestBody: " + form;
     }
 
     @Data
-    public static class RequestForm {
+    public static class RequestBodyForm {
 
         private String value;
     }
