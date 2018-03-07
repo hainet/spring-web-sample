7037d2c0d0b285b0d7321ee1066c674ed79f15bd
hainet <20400616+hainet@users.noreply.github.com>
2018-03-07 14:16:55 +0900

    ResponseEntityを返却することでHTTPステータスコードを指定するサンプルを追加

--- src/main/java/com/hainet/spring/web/sample/web/HttpStatusController.java
+++ src/main/java/com/hainet/spring/web/sample/web/HttpStatusController.java
@@ -2,7 +2,9 @@ package com.hainet.spring.web.sample.web;
 
 import com.hainet.spring.web.sample.web.exception.HttpStatus404Exception;
 import com.hainet.spring.web.sample.web.exception.HttpStatus500Exception;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -32,16 +34,12 @@ public class HttpStatusController {
     @GetMapping("/http-servlet-response/{httpStatus}")
     public void handleHttpStatusByHttpServletResponse(
             final HttpServletResponse response,
-            @PathVariable final String httpStatus) {
-        switch (httpStatus) {
-            case "404":
-                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                break;
-            case "500":
-                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                break;
-            default:
-        }
+            @PathVariable final int httpStatus) {
+        response.setStatus(httpStatus);
     }
 
+    @GetMapping("/response-entity/{httpStatus}")
+    public ResponseEntity<String> responseEntity(@PathVariable final int httpStatus) {
+        return new ResponseEntity<>(new HttpHeaders(), HttpStatus.valueOf(httpStatus));
+    }
 }
