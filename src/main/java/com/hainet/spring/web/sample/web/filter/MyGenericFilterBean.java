201a3be8f960114325a2b6d446d84043a9db241b
hainet <20400616+hainet@users.noreply.github.com>
2018-04-02 19:53:04 +0900

    GenericFilterBeanとOncePerRequestFilterについて拡張ポイントの追記

--- src/main/java/com/hainet/spring/web/sample/web/filter/MyGenericFilterBean.java
+++ src/main/java/com/hainet/spring/web/sample/web/filter/MyGenericFilterBean.java
@@ -1,5 +1,7 @@
 package com.hainet.spring.web.sample.web.filter;
 
+import org.springframework.beans.BeanWrapper;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -15,10 +17,27 @@ public class MyGenericFilterBean extends GenericFilterBean {
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
         System.out.println("-- " + this.getClass().getSimpleName() + "#doFilter");
-        System.out.println(super.getEnvironment());
-        System.out.println(super.getFilterConfig());
+        System.out.println("super#getEnvironment: " + super.getEnvironment());
+        System.out.println("super#getFilterConfig: " + super.getFilterConfig());
         System.out.println("Before chain.doFilter in " + this.getClass().getSimpleName() + "#doFilter");
         chain.doFilter(request, response);
         System.out.println("After chain.doFilter in " + this.getClass().getSimpleName() + "#doFilter");
+    }
+
+    @Override
+    public Environment createEnvironment() {
+        System.out.println("-- " + this.getClass().getSimpleName() + "#createEnvironment");
+        return super.createEnvironment();
+    }
+
+    @Override
+    public void initBeanWrapper(final BeanWrapper bw) {
+        System.out.println("-- " + this.getClass().getSimpleName() + "#initBeanWrapper");
+        System.out.println("BeanWrapper: " + bw);
+    }
+
+    @Override
+    public void initFilterBean() {
+        System.out.println("-- " + this.getClass().getSimpleName() + "#initFilterBean");
     }
 }
--- src/main/java/com/hainet/spring/web/sample/web/filter/MyOncePerRequestFilter.java
+++ src/main/java/com/hainet/spring/web/sample/web/filter/MyOncePerRequestFilter.java
@@ -18,14 +18,47 @@ public class MyOncePerRequestFilter extends OncePerRequestFilter {
             final HttpServletResponse response,
             final FilterChain filterChain) throws IOException, ServletException {
         System.out.println("-- " + this.getClass().getSimpleName() + "#doFilterInternal");
-        System.out.println(super.getAlreadyFilteredAttributeName());
         System.out.println("Before chain.doFilter in " + this.getClass().getSimpleName() + "#doFilterInternal");
         filterChain.doFilter(request, response);
         System.out.println("After chain.doFilter in " + this.getClass().getSimpleName() + "#doFilterInternal");
     }
 
     @Override
+    public boolean isAsyncDispatch(final HttpServletRequest request) {
+        System.out.println("-- " + this.getClass().getSimpleName() + "#isAsyncDispath");
+        System.out.println("HttpServletRequest: " + request);
+        return super.isAsyncDispatch(request);
+    }
+
+    @Override
+    public boolean isAsyncStarted(final HttpServletRequest request) {
+        System.out.println("-- " + this.getClass().getSimpleName() + "#isAsyncStarted");
+        System.out.println("HttpServletRequest: " + request);
+        return super.isAsyncStarted(request);
+    }
+
+    @Override
+    public String getAlreadyFilteredAttributeName() {
+        System.out.println("-- " + this.getClass().getSimpleName() + "#isAsyncStarted");
+        return super.getAlreadyFilteredAttributeName();
+    }
+
+    @Override
     public boolean shouldNotFilter(final HttpServletRequest request) {
+        System.out.println("-- " + this.getClass().getSimpleName() + "#shouldNotFilter");
+        System.out.println("HttpServletRequest: " + request);
         return false;
+    }
+
+    @Override
+    public boolean shouldNotFilterAsyncDispatch() {
+        System.out.println("-- " + this.getClass().getSimpleName() + "#shouldNotFilterAsyncDispatch");
+        return super.shouldNotFilterAsyncDispatch();
+    }
+
+    @Override
+    public boolean shouldNotFilterErrorDispatch() {
+        System.out.println("-- " + this.getClass().getSimpleName() + "#shouldNotFilterErrorDispatch");
+        return super.shouldNotFilterErrorDispatch();
     }
 }
